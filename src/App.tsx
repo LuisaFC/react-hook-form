2d227b5b8daef5bff2f1e384e82eaa55f3acfc4d
LuisaFC <luisabsb@gmail.com>
2025-01-24 15:54:36 -0300

    correct indent size

--- src/App.tsx
+++ src/App.tsx
@@ -9,64 +9,69 @@ interface IFormData {
 }
 
 function App() {
-    console.log("App renderizou")
+  console.log("App renderizou")
 
-    //handleSubmit -> submit function
-    //register -> input register anda validation 
-    //formState -> form infos
+  //handleSubmit -> submit function
+  //register -> input register anda validation
+  //formState -> form infos
 
-    const {handleSubmit: hookFormHandleSubmit, register, formState} = useForm<IFormData>()
+  const {handleSubmit: hookFormHandleSubmit, register, formState} = useForm<IFormData>()
 
-    const handleSubmit = hookFormHandleSubmit(
-		(data) => {
-			console.log("Esse é o onValid")
-			console.log("data", {data: data})
-		},
-		(errors) => {
-			console.log("errors", errors)
-		}
+  const handleSubmit = hookFormHandleSubmit(
+    (data) => {
+      console.log("Esse é o onValid")
+      console.log("data", {data: data})
+    },
+    (errors) => {
+      console.log("errors", errors)
+    }
 	)
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-        <form
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-2 w-96"
-        >
-            <div>
-                <Input 
-                    placeholder="Nome"
-                    {...register('name',{
-                        required: {
-                            value: true,
-                            message: "O nome é obrigatório"
-                        }
-                    })}
-                />
-                <ErrorMessage
-                    errors={formState.errors}
-                    name="name"
-                    render={({ message }) => <small className="text-red-400 block">{message}</small>} 
-                />
-            </div>
+      <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-2 w-96"
+      >
+        <div>
+          <Input
+            placeholder="Nome"
+            {...register('name',{
+              required: {
+                value: true,
+                message: "O nome é obrigatório"
+              }
+            })}
+          />
+          <ErrorMessage
+            errors={formState.errors}
+            name="name"
+            render={({ message }) => <small className="text-red-400 block">{message}</small>}
+          />
+        </div>
 
-            <div>
-                <Input 
-                    type="number"
-                    placeholder="Idade"
-                    {...register('age')}
-                />
-                <ErrorMessage
-                    errors={formState.errors}
-                    name="age"
-                    render={({ message }) => <small className="text-red-400 block">{message}</small>} 
-                />
-            </div>
+        <div>
+          <Input
+            type="number"
+            placeholder="Idade"
+            {...register('age', {
+              required: {
+                value: true,
+                message: "O nome é obrigatório"
+              }
+            })}
+          />
+          <ErrorMessage
+            errors={formState.errors}
+            name="age"
+            render={({ message }) => <small className="text-red-400 block">{message}</small>}
+          />
+        </div>
 
-            <Button className="mt-4">
-                Enviar
-            </Button>
-        </form>
+        <Button className="mt-4">
+          Enviar
+        </Button>
+      </form>
     </div>
   );
 }
